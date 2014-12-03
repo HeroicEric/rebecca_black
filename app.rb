d167c5c15d261d96eb489feddabfcddeca2ddb16
Eric Kelly <heroiceric@gmail.com>
2014-12-03 13:20:00 -0500

    Allow routes to be registered Sinatra style

--- app.rb
+++ app.rb
@@ -1,28 +1,48 @@
 require 'rack'
 require 'erb'
 require 'pry'
+require 'ostruct'
 
-def routes
-  {
-    '/' => Proc.new do
-      derp =  'hello'
-      template = ERB.new(File.read('views/index.erb'))
-      html = template.result(binding)
-
-      [200, {}, [html]]
-    end
-  }
+class Template < OpenStruct
+  def render(template)
+    template = ERB.new(File.read(template))
+    template.result(binding)
+  end
 end
 
-def find_route(path)
-  routes[path]
+ROUTES = {
+  GET: {}
+}
+
+def not_found
+  [404, {}, ["<h1>GTFO</h1>"]]
+end
+
+def get(path, &block)
+  ROUTES[:GET][path] = block
+end
+
+def erb(template, locals={})
+  template_path = "views/#{template}.erb"
+  Template.new(locals).render(template_path)
 end
 
 def respond(env)
   path = env['PATH_INFO']
+  request_method = env["REQUEST_METHOD"].to_sym
 
-  route = find_route(path)
-  route ? route.call : [404, {}, ["<h1>GTFO</h1>"]]
+  route = ROUTES[request_method][path]
+
+  if route
+    body = route.call
+    [200, {}, [body]]
+  else
+    not_found
+  end
+end
+
+get "/spencer" do
+  erb :index, { derp: 'hello spencer' }
 end
 
 Rack::Handler::WEBrick.run method(:respond)
